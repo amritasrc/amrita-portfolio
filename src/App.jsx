b2671a8e31704e5c12258be315f53147a97648d1
amritasrc <amritakumari13022006@gmail.com>
2026-02-15 19:43:54 +0530

    Done with about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Intro from './Comps/Intro'
 import Nav from './Comps/Nav'
+import Stack from './Comps/Stack'
+import About from './Comps/About'
 
 const App = () => {
   return (
     <div className='bg-black text-white font-sans '>
 
-      <main className='relative min-h-screen max-w-3xl mx-auto py-8 border border-zinc-900 overflow-hidden '>
+      <main className='relative min-h-screen max-w-3xl mx-auto py-8 border border-zinc-800 overflow-hidden '>
         <Nav />
         <Intro />
+        <Stack/>
+        <About/>
       </main>
 
     </div>
---
+++ src/Comps/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className='py-8 border-b border-zinc-800'>
+
+            <h2 className='pl-5 py-1.5 font-semibold text-xl border-b border-t border-zinc-800'>
+                About me
+            </h2>
+            <div className='px-5 my-3 text-zinc-400'>
+                Turning ideas into functional, clean, and engaging software — I’m a BTech student exploring full-stack development and modern technologies.
+                <br /> <br />
+                I love experimenting with new tools, frameworks, and creative solutions to bring concepts to life.
+            </div>
+        </section>
+    )
+}
+
+export default About
