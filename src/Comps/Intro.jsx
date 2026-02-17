a46c0af2dadc9ade24aa8c84ea72965929e4ce1b
amritasrc <amritakumari13022006@gmail.com>
2026-02-17 18:51:38 +0530

    Fixed mobile view of intro section

--- src/Comps/Intro.jsx
+++ src/Comps/Intro.jsx
@@ -9,31 +9,41 @@ import Button from './Button';
 
 const Intro = () => {
     return (
-        <section className='py-8 border-b border-zinc-800'>
+       <section className="py-8 border-b border-zinc-800">
+  <div className="flex mt-2 items-start">
+    
+    <div className="px-4 shrink-0">
+      <img
+        src="/profile.jpg"
+        alt="Profile-img"
+        className="w-20 h-20 rounded-full object-cover border border-zinc-700"
+      />
+    </div>
 
-            <div className='flex mt-2'>
-                <div className='pl-5 pr-5'>
-                    <img src="/profile.jpg" alt="Profile-img" className='w-21 h-20 rounded-full object-cover border border-zinc-700' />
-                </div>
+    <div className="pr-4">
+      <h2 className="text-2xl font-semibold">Hello, I'm Amrita!</h2>
+      <h2 className="text-zinc-400">Frontend Web Developer</h2>
 
-                <div>
-                    <h2 className='text-2xl font-semibold'>Hello, I'm Amrita!</h2>
-                    <h2 className='text-zinc-400'>Frontend Web Developer</h2>
-                    <p className="text-zinc-200 text-xl mt-2 leading-tight">Building clean, responsive web interfaces with care and curiosity.</p>
+      <p className="text-zinc-200 text-base sm:text-xl mt-2 leading-tight">
+        Building clean, responsive web interfaces with care and curiosity.
+      </p>
 
-                    <p className='flex items-center gap-2 text-zinc-400 mt-3'><LuPin className='mt-1.5' /><span>Ranchi, Jharkhand</span></p>
+      <p className="flex items-center gap-2 text-zinc-400 mt-3">
+        <LuPin className="mt-1" />
+        <span>Ranchi, Jharkhand</span>
+      </p>
 
-                    <div className='flex gap-3 mt-3'>
+      <div className="flex gap-3 mt-3 flex-wrap">
+        <Button Icon={FaGithub} />
+        <Button Icon={BsTwitterX} />
+        <Button Icon={FaLinkedin} />
+        <Button Icon={FaDiscord} />
+      </div>
+    </div>
 
-                        <Button Icon={FaGithub} />
-                        <Button Icon={BsTwitterX}/>
-                        <Button Icon={FaLinkedin}/>
-                        <Button Icon={FaDiscord}/>
+  </div>
+</section>
 
-                    </div>
-                </div>
-            </div>
-        </section>
     )
 }
 
