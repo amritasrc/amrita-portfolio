9de743d9c246a37a2130489758ed9380bcc2b13b
amritasrc <amritakumari13022006@gmail.com>
2026-02-18 11:38:31 +0530

    Added target blank for social links

--- src/Comps/Button.jsx
+++ src/Comps/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({Icon, link}) => {
     return (
-        <a href={link} className='px-4 py-2 bg-[#1f1b17] hover:bg-zinc-800 rounded-xl cursor-pointer transition-opacity duration-200 ease-in-out'>
+        <a href={link} target='_blank' className='px-4 py-2 bg-[#1f1b17] hover:bg-zinc-800 rounded-xl cursor-pointer transition-opacity duration-200 ease-in-out'>
            <Icon/>
         </a>
     )
