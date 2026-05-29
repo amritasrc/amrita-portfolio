450174fd4b4a838758aabc4d0387a68a005f508a
amritasrc <amritakumari13022006@gmail.com>
2026-05-29 20:55:11 +0530

    fix: replace text cursor with default cursor on hover

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from './Comps/Contact'
 
 const App = () => {
   return (
-    <div className='bg-zinc-50 text-zinc-900 dark:bg-[#12100e] dark:text-zinc-100 min-h-screen transition-colors duration-300'>
+    <div className='normal-cursor bg-zinc-50 text-zinc-900 dark:bg-[#12100e] dark:text-zinc-100 min-h-screen transition-colors duration-300'>
 
       <main className='relative min-h-screen max-w-3xl mx-auto pt-8 border border-zinc-300 dark:border-zinc-800 overflow-hidden'>
         <Nav />
