8bd0f582c75ba31a88ba9ddd96ec08eda8edc61e
amritasrc <amritakumari13022006@gmail.com>
2026-05-29 18:58:10 +0530

    style: add dark mode layout styling to App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ import Contact from './Comps/Contact'
 
 const App = () => {
   return (
-    <div className='bg-[#12100e] text-white font-sans '>
+    <div className='bg-zinc-50 text-zinc-900 dark:bg-[#12100e] dark:text-zinc-100 min-h-screen transition-colors duration-300'>
 
-      <main className='relative min-h-screen max-w-3xl mx-auto pt-8 border border-zinc-800 overflow-hidden '>
-        {/* <Nav /> */}
+      <main className='relative min-h-screen max-w-3xl mx-auto pt-8 border border-zinc-300 dark:border-zinc-800 overflow-hidden'>
+        <Nav />
         <Intro />
-        <About/>
-        <Project/>
-        <Contact/>
+        <About />
+        <Project />
+        <Contact />
       </main>
 
     </div>
