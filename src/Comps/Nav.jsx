9707c75d59e576cfd7b8c6bfdd29af440c885e1b
amritasrc <amritakumari13022006@gmail.com>
2026-05-29 18:56:34 +0530

    feat: add theme toggle buttons in navbar

--- src/Comps/Nav.jsx
+++ src/Comps/Nav.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import { WiDaySunny } from "react-icons/wi";
 import { IoMoon } from "react-icons/io5";
-import { HiOutlineDesktopComputer } from "react-icons/hi";
+
+import { useTheme } from '../context/ThemeContext';
 
 const Nav = () => {
+
+    const { theme, setTheme } = useTheme();
+
     return (
-        <nav className='flex justify-end mb-0.5 pr-6 gap-5'>
-            <span><WiDaySunny /></span>
-            <span><IoMoon /></span>
-            <span><HiOutlineDesktopComputer /></span>
+        <nav className='flex justify-end mb-0.5 pr-6 gap-5 text-2xl'>
+
+            <button
+                onClick={() => setTheme("light")}
+                className={`${theme === "light" ? "opacity-100" : "opacity-50"}`}
+            >
+                <WiDaySunny className='w-6'/>
+            </button>
+
+            <button
+                onClick={() => setTheme("dark")}
+                className={`${theme === "dark" ? "opacity-100" : "opacity-50"}`}
+            >
+                <IoMoon className='w-5'/>
+            </button>
+
         </nav>
     )
 }
