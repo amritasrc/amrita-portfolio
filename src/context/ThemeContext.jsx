5fe6973ab4ca1f46e01eb9bed34a5c21a17b0488
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-05-30 20:56:22 +0530

    style: #1 github contribution light mode

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,33 +3,32 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
+  const [resolvedTheme, setResolvedTheme] = useState("light");
 
-    const [theme, setTheme] = useState(() => {
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
 
-        const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) return savedTheme;
 
-        if (savedTheme) return savedTheme;
+    return "dark";
+  });
 
-        return "dark";
-    });
+  useEffect(() => {
+    const root = window.document.documentElement;
 
-    useEffect(() => {
+    root.classList.remove("light", "dark");
 
-        const root = window.document.documentElement;
+    root.classList.add(theme);
 
-        root.classList.remove("light", "dark");
+    localStorage.setItem("theme", theme);
+    setResolvedTheme(theme);
+  }, [theme]);
 
-        root.classList.add(theme);
-
-        localStorage.setItem("theme", theme);
-
-    }, [theme]);
-
-    return (
-        <ThemeContext.Provider value={{ theme, setTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    );
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme, resolvedTheme: theme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
 export const useTheme = () => useContext(ThemeContext);
