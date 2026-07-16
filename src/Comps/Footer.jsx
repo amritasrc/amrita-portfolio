ae93eb436d5f8cc6f55eaddac9075c140e0a083f
amritasrc <amritakumari13022006@gmail.com>
2026-07-16 19:52:17 +0530

    style: made few changes

--- src/Comps/Footer.jsx
+++ src/Comps/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="py-12 text-center text-xs text-zinc-300 dark:text-zinc-500 border-t border-zinc-300 dark:border-zinc-600">
+    <footer className="py-12 text-center text-xs text-zinc-500 dark:text-zinc-400 border-t border-zinc-500 dark:border-zinc-500">
       <p className="mb-2">© 2026 Amrita Kumari</p>
       <p>Made with lots of coffee and curiosity.</p>
     </footer>
